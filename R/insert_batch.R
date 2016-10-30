f33e7db0f4839a8cef32e8f02efc785eb89aaa72
jwinternheimer <julianwinternheimer@gmail.com>
2016-10-30 19:20:26 -0400

    Fixed insert_batch function.

--- R/insert_batch.R
+++ R/insert_batch.R
@@ -11,19 +11,13 @@
 #' con <- redshift_connect()
 #' insert_batch(con, "mrr_forecast", forecast_df);
 
-## Define helper functions
-create_empty_table <- function(con, tn, df) {
+insert_batch <- function(con, table_name, df, size=100L) {
 
   sql <- paste0("create table \"",tn,"\" (",paste0(collapse=',','"',names(df),'" ',sapply(df[0,],postgresqlDataType)),");")
+  print(sql)
 
   dbSendQuery(con,sql)
   invisible()
-
-}
-
-insert_batch <- function(con, table_name, df, size=100L) {
-
-  create_empty_table(con, table_name, df)
 
   if (nrow(df)==0L) return(invisible());
 
@@ -31,6 +25,7 @@ insert_batch <- function(con, table_name, df, size=100L) {
 
   for (i in seq(0L,len=cnt)) {
     sql <- paste0("insert into \"",table_name,"\" values (",do.call(paste,c(sep=',',collapse='),(',lapply(df[seq(i*size+1L,min(nrow(df),(i+1L)*size)),],shQuote))),");");
+    print(sql)
     dbSendQuery(con,sql);
   };
 
